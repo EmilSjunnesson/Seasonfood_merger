c4d8658ae49235131b51192a0df5ccd06097cb50
Emil Sjunnesson <byrhult@gmail.com>
2013-03-25 15:44:34 +0100

    comented on myline

--- src/petter/bjelm/MyLine.java
+++ src/petter/bjelm/MyLine.java
@@ -6,25 +6,13 @@ import java.awt.Graphics;
 import javax.swing.JComponent;
 
 public class MyLine extends JComponent {
-	private Color c;
 	public MyLine() {
-		// TODO Auto-generated constructor stub
+
 	}
-	
+	//draws a black line
 	protected void paintComponent(Graphics g){
-		if (c==null){
-			c = Color.BLUE;	
-		}
-
-		//g.setColor(Color.BLUE);
-		g.setColor(c);
-		
-		//g.draw3DRect(0, 0, getWidth(), getHeight(), true);
-
 		g.setColor(Color.BLACK);
 		g.drawLine(0, 0, 0, getHeight());
-		//behövs inte här
-		super.paintComponent(g);
 	}
 
 }
