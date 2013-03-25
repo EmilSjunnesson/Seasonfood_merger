27212b921d963f1a9151727d65681211b8d07801
Emil Sjunnesson <byrhult@gmail.com>
2013-03-25 16:06:49 +0100

    comment Getimgfrom URL

--- src/petter/bjelm/GetImgFromURL.java
+++ src/petter/bjelm/GetImgFromURL.java
@@ -10,13 +10,14 @@ public class GetImgFromURL {
 	public GetImgFromURL() {
 
 	};
-
+	
+	//creates image object from URL
 	public BufferedImage getURLImage(String imgURL) {
 		BufferedImage image = null;
 		try {
 			URL url = new URL(imgURL);
 			image = ImageIO.read(url);
-			
+
 		} catch (Exception exp) {
 
 		}
