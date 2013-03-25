bb1122d4eecc68ad1ade1a1a1bb1ba70ab3e396f
Emil Sjunnesson <byrhult@gmail.com>
2013-03-25 16:17:37 +0100

    coment getcategory

--- src/petter/bjelm/GetCategory.java
+++ src/petter/bjelm/GetCategory.java
@@ -14,41 +14,35 @@ import org.xml.sax.SAXException;
 
 public class GetCategory {
 
-	public ArrayList<String> sortArray = new ArrayList<String>();
-	Document doc;
-	Node nNode;
-	NodeList nList;
-	Element eElement;
+	private ArrayList<String> sortArray = new ArrayList<String>();
+	private Document doc;
+	private Node nNode;
+	private NodeList nList;
 
 	public GetCategory() {
 
 	}
-
+	
+	//Takes the xml document returns an array of food names in season
 	public ArrayList<String> getArray(Document docIn) throws IOException,
 			ParserConfigurationException, SAXException, TransformerException {
 
+		// clears the array every time the code runs (button is pressed)
 		sortArray.clear();
-
+		
 		doc = docIn;
 		
 		nList = doc.getElementsByTagName("results");
 
 		for (int temp = 0; temp < nList.getLength(); temp++) {
-
 			nNode = nList.item(temp);
 
-			// System.out.println("\nCurrent Element :" + nNode.getNodeName());
-
 			for (int temp2 = 0; temp2 < nNode.getChildNodes().getLength(); temp2++) {
-
 				sortArray.add(temp2, ""
 						+ nNode.getChildNodes().item(temp2).getNodeName());
-
 			}
 		}
-
 		return sortArray;
-
 	}
 
 }
